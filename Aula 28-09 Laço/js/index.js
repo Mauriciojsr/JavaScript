495eec55d7f978acc2b8710370a994e6ae1a1bb9
Mauricio Jose da Silva Ramos <mauriciojsr@hotmail.com>
2022-09-28 09:27:23 -0300

    Commit

--- Aula 28-09 Laço/js/index.js
+++ Aula 28-09 Laço/js/index.js
@@ -17,3 +17,17 @@ var btn = document.getElementById('btn');
 btn.addEventListener("click", function(){
     alert('Total Selecionados: ' + howMany(document.selectForm.tipoMusica))
 });
+
+/*do...while
+do
+    Stat
+while(condição)*/
+
+var resultado = '';
+var i = 0;
+
+do{
+    i+=1;
+    resultado += i + '';
+}while(i<5);
+document.getElementById('exemplo').innerHTML = resultado;
