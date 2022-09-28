b628d553ef2ca37c891e6add64e11c9e190fafdd
Mauriciojsr <mauriciojsr@hotmail.com>
2022-09-28 10:27:00 -0300

    Commit

--- Aula 28-09 Laço/js/index.js
+++ Aula 28-09 Laço/js/index.js
@@ -31,3 +31,33 @@ do{
     resultado += i + '';
 }while(i<5);
 document.getElementById('exemplo').innerHTML = resultado;
+
+//Elemento de repetição com funcionalidade semelhante  suas diferenças
+
+//=> while - Execua o loop enquanto a condição for verdadeira
+//Contar de 1 até 10
+contagem = 0;
+var i = 0;
+while(contar <= 10){
+    i++;
+}
+
+//Do while - Executa o Loop primeiro e depois verifica a condição
+do{
+    contagem ++;
+}while(contagem <= 10);
+
+//For - Executa o loop enquanto a condição for verdadeira, porem a instancia das variávis de contagem são instanciadas na estrutura loop!
+
+contagem = 0;
+var i = 0;
+for(i = 0; i <=10; i++){
+    contagem++;
+}
+
+//Foreach - Executa o loop sobre elementos de um array
+contagem = [0,1,2,3,4,5,6,7,8,9];
+i=0;
+array.forEach(element => {
+    
+});
